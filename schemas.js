6c4dcbd8f4e934331d5e62727862fe33913abc34
pascal7777 <44567928+pascal7777@users.noreply.github.com>
2023-05-14 22:56:50 +0700

    Update schemas.js

--- schemas.js
+++ schemas.js
@@ -27,8 +27,6 @@ const Joi = BaseJoi.extend(extension);
 module.exports.campgroundSchema = Joi.object({
   campground: Joi.object({
     title: Joi.string().required().escapeHTML(),
-    price: Joi.number().required().min(0),
-    overnigth_price: Joi.number().min(0),
     location: Joi.string().required().escapeHTML(),
     description: Joi.string().required().escapeHTML(),
   }).required(),
