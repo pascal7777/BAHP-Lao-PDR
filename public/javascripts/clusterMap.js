d2e01d9aa4d35e6762c36d2c9965fb61b3d96461
pascal7777 <44567928+pascal7777@users.noreply.github.com>
2023-05-16 22:18:19 +0700

    Update clusterMap.js

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "cluster-map",
-  style: "mapbox://styles/mapbox/light-v10",
+  style: "mapbox://styles/mapbox/satellite-streets-v12",
   center: [104.764466, 16.571351],
   zoom: 4,
 });
